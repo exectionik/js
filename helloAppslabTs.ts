fc5b5dc3dbac38bb8b9e0440a0ef1224e1c9591b
exectionik <exectionik9@gmail.com>
2020-10-19 11:07:39 +0200

    finally

--- helloAppslabTs.ts
+++ helloAppslabTs.ts
@@ -1,1 +1,5 @@
-console.log("Hello Appslab 2020!")
+function total(heightS: number, lenghtS: number, heightT: number): number {
+    return((heightS + lenghtS) * (heightT/heightS))
+}
+console.log(total(1, 2, 100))   
+   
---
+++ out/helloAppslabTs.js
@@ -0,0 +1,5 @@
+function total(heightS, lenghtS, heightT) {
+    return ((heightS + lenghtS) * (heightT / heightS));
+}
+console.log(total(1, 2, 100));
+//# sourceMappingURL=helloAppslabTs.js.map
